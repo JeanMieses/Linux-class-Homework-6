5f4148feac2c46c8369d420b3e555506c0eb158a
Jean <araujo0275@gmail.com>
2020-09-21 04:41:47 +0000

    My favorite food is...

--- homework6.java
+++ homework6.java
@@ -1,7 +1,7 @@
 public class homework6{
 	public static void main(String[] args){
 	
-		System.out.println("I am taking cs407 because I wanted to know more about linux and it is usually use by programmers");
+		System.out.println("My favorite food is Sushi");
 	
 	}
 }
