d84a7d53caa46ed0868c46da18d001713793f5fc
Jean <araujo0275@gmail.com>
2020-09-21 04:37:16 +0000

    My favorite programming language is...

--- homework6.java
+++ homework6.java
@@ -1,7 +1,7 @@
 public class homework6{
 	public static void main(String[] args){
 	
-		System.out.println("My Name is Jean Carlos");
+		System.out.println("My favorite programming language is Java!");
 	
 	}
 }
